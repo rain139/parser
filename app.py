c74b60c8938e71dbcd104b73d330a0452dc8c47d
rain139 <rain139@ukr.net>
2019-05-04 12:54:31 +0300

    restail

--- app.py
+++ app.py
@@ -2,8 +2,26 @@ from src.Parsers.EmailParser import EmailParser
 from src.services.db import db
 import sys
 
-
 sys.setrecursionlimit(100000)
 
+count = len(sys.argv)
 
-EmailParser('https://anje.com.ua','anje').run()
+if count > 1:
+    domen = sys.argv[1]
+else:
+    exit("Error not find arg DOMEN")
+
+if count > 2:
+    table = sys.argv[2]
+else:
+    exit("Error not find arg table")
+
+config = {}
+
+if count > 3:
+    for key, item in enumerate(sys.argv):
+        if key > 2:
+            key_value = item.split('=')
+            config[key_value.pop()] = key_value.pop()
+
+EmailParser(domen, table, **config).run()
--- src/DomenParserAbstractFactory.py
+++ src/DomenParserAbstractFactory.py
@@ -9,24 +9,33 @@ class Parser(object):
     __url = []
     __url_tmp = []
 
+    _save_link = False
+
     __metaclass__ = abc.ABCMeta
 
     site_url = None
 
+    _special_link = ''
+
     _table = None
 
-    def __init__(self, site_url: str, table: str):
+    def __init__(self, site_url: str, table: str, **kwargs):
         self._table = table
         self._create_table()
         self.site_url = site_url.strip('/')
+
+        if kwargs.get('savve_link', False):
+            self._save_link = kwargs.get('savve_link', False)
+        if kwargs.get('special_link', False):
+            self._special_link = kwargs.get('special_link', '').strip('/')
 
     def _create_table(self):
         cursor = db().connect().cursor()
         try:
             cursor.execute("CREATE TABLE IF NOT EXISTS  `{table}`"
-                       "( `id` INT NOT NULL AUTO_INCREMENT ,"
-                       " `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,"
-                       " PRIMARY KEY (`id`)) ENGINE = InnoDB;".format(table=self._table))
+                           "( `id` INT NOT NULL AUTO_INCREMENT ,"
+                           " `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,"
+                           " PRIMARY KEY (`id`)) ENGINE = InnoDB;".format(table=self._table))
             db().connect().commit()
         except Exception:
             exit('\n \033[91m Error create table `{table}` \033[0m \n'.format(table=self._table))
@@ -45,7 +54,10 @@ class Parser(object):
                 exit()
 
         soup = BeautifulSoup(html, features='html.parser')
-        all_tag_a = list(set(soup.findAll('a')))
+        if self._special_link:
+            all_tag_a = list(set(soup.findAll('a', href=re.compile("^(/{href}/)".format(href=self._special_link)))))
+        else:
+            all_tag_a = list(set(soup.findAll('a')))
 
         print('tmp_link = {tmp_link} all_link: {all_link}'.format(tmp_link=self.__url_tmp.__len__(),
                                                                   all_link=self.__url.__len__()))
@@ -67,6 +79,11 @@ class Parser(object):
                 self.__url_tmp.append(href)
                 self._action(cursor, soup)
 
+                if self._save_link:
+                    try:
+                        cursor.execute("INSERT INTO `links` (`name`) VALUES (%s)", [href])
+                    except:
+                        print('Error insert links')
         db().connect().commit()
 
         if self.__url_tmp:
