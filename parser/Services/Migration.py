a382ba5f073458112816d84fb25d601e6c42b2ed
rain139 <rain139@ukr.net>
2019-05-12 12:57:47 +0300

    add migration

--- parser/Services/Migration.py
+++ parser/Services/Migration.py
@@ -1,3 +1,21 @@
+from parser.Services.Db import Db
+
+
 class Migration:
+    __sql_migration = []
+
+    def __init__(self):
+        self.__set_sql_migration()
+
+    def __set_sql_migration(self):
+        self.__sql_migration = [
+            "CREATE TABLE IF NOT EXISTS `parser_site` ( `id` INT NOT NULL AUTO_INCREMENT , `site` VARCHAR(255) NULL DEFAULT NULL , `tb` VARCHAR(255) NULL DEFAULT NULL , `special_link` VARCHAR(255) NULL DEFAULT NULL , `parse` INT(1) NOT NULL DEFAULT '0' , `process` INT(1) NOT NULL DEFAULT '0' , `links` INT(11) NOT NULL DEFAULT '0' , `start` TIMESTAMP NULL DEFAULT NULL , `end` TIMESTAMP NULL DEFAULT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;"
+        ]
+
     def run(self):
-        pass
+        if self.__sql_migration:
+            cursor = Db().connect().cursor()
+            for migration in self.__sql_migration:
+                cursor.execute(migration)
+            Db().connect().commit()
+            cursor.close()
