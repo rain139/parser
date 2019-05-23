95c61d064d507ba36c87476ba2c8e0d9e0957465
rain139 <rain139@ukr.net>
2019-05-23 22:54:06 +0300

    ff

--- parser/Services/Db.py
+++ parser/Services/Db.py
@@ -7,7 +7,7 @@ class Db:
 
     def __init__(self):
         if not self.__instance:
-            D_instance = Db.__Mysql().connect()
+            Db.__instance = Db.__Mysql().connect()
 
     def connect(self) -> mysql:
         return Db.__instance
