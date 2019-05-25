03398e6f4cd05ac92e69bd38aac65b00fa680bd6
rain139 <rain139@ukr.net>
2019-05-25 12:45:04 +0300

    table parse

--- parser/Services/TableParseSite.py
+++ parser/Services/TableParseSite.py
@@ -20,7 +20,7 @@ def set_result_parse(id: int, count_links: int) -> None:
 
 def save_count_links(table: str, count_links: int) -> None:
     cursor = Db().connect().cursor()
-    cursor.execute('UPDATE `parser_site` SET `links` = %s WHERE `table` = %s',
+    cursor.execute('UPDATE `parser_site` SET `links` = %s WHERE `tb` = %s',
                    [count_links, table])
     Db().connect().commit()
 
